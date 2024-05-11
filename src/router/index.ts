de2ca6a17655c7e68a98cb2e2a736068e7242440
Niels Ouvrard <niels.ouvrard@epitech.eu>
2024-05-11 03:38:20 +0200

    prettier

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,51 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: "/",
-    name: "home",
+    path: '/',
+    name: 'home',
     component: HomeView,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/AboutView.vue'),
+  },
+  {
+    path: '/users',
+    name: 'users',
+    component: () => import('../views/UsersView.vue'),
+  },
+  {
+    path: '/user/:id',
+    children: [
+      {
+        path: '',
+        component: () => import('../views/userView.vue'),
+      },
+    ],
+  },
+  {
+    path: '/messages',
+    name: 'messages',
+    component: () => import('../views/MessagesView.vue'),
+  },
+  {
+    path: '/message/:id',
+    children: [
+      {
+        path: '',
+        component: () => import('../views/messageView.vue'),
+      },
+    ],
+  },
+  {
+    path: '/:catchAll(.*)',
+    component: () => import('../views/NotFoundView.vue'),
   },
 ];
+// put a link to profile in the user view
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
---
+++ src/types/types.ts
@@ -0,0 +1,16 @@
+export interface UserType {
+  id: number;
+  username: string;
+  email: string;
+  password: string;
+  created_at: string;
+  relationship?: number[];
+}
+
+export interface Message {
+  id: number;
+  sender_id: number;
+  recipient_id: number;
+  message: string;
+  sent_at: string;
+}
