cd2c62f2829bc9f805fd1d5302abfa37284922c2
Niels Ouvrard <niels.ouvrard@epitech.eu>
2024-05-11 13:31:21 +0200

    Add test case for backend response validation

--- tests/test.py
+++ tests/test.py
@@ -1,10 +1,47 @@
 import requests
+import json
 
 def test_backend(url):
+    expected = json.loads("""
+                [
+                {
+                    "id": 1,
+                    "username": "user1",
+                    "email": "user1@example.com",
+                    "created_at": "2024-05-11 12:38:05"
+                },
+                {
+                    "id": 2,
+                    "username": "user2",
+                    "email": "user2@example.com",
+                    "created_at": "2024-05-11 12:38:05"
+                },
+                {
+                    "id": 3,
+                    "username": "user3",
+                    "email": "user3@example.com",
+                    "created_at": "2024-05-11 12:38:05"
+                },
+                {
+                    "id": 4,
+                    "username": "user4",
+                    "email": "user4@example.com",
+                    "created_at": "2024-05-11 12:38:05"
+                },
+                {
+                    "id": 5,
+                    "username": "user5",
+                    "email": "user5@example.com",
+                    "created_at": "2024-05-11 12:38:05"
+                }
+                ]
+                """)
     response = requests.get(url)
     if response.status_code == 200:
-        print("Backend is working properly.")
-        print("Response:", response)
+        if json.loads(response.text) == expected:
+            print("Backend is working properly.")
+        else:
+            print("There seems to be an issue with the backend.")
     else:
         print("There seems to be an issue with the backend.")
 
