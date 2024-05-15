63427e1318bbeaa83402670f03934849c10081ce
Niels Ouvrard <niels.ouvrard@epitech.eu>
2024-05-15 23:51:01 +0200

    vuex and fixes

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,9 +1,34 @@
+import axios from 'axios';
 import { createStore } from 'vuex';
 
 export default createStore({
-  state: {},
+  state: {
+    jwt: null,
+  },
   getters: {},
-  mutations: {},
-  actions: {},
+  mutations: {
+    setJwt(state, jwt) {
+      state.jwt = jwt;
+    },
+  },
+  actions: {
+    async login({ commit }, credentials) {
+      const response = await axios.post(
+        'http://ouvrard.niels.free.fr/index.php?/login',
+        //'http://localhost:8000/index.php?/login',
+        {
+          email: credentials.email,
+          password: credentials.password,
+        },
+      );
+
+      if (!response.data.token) {
+        throw new Error('Invalid credentials');
+      }
+
+      const jwt = await response.data.token;
+      commit('setJwt', jwt);
+    },
+  },
   modules: {},
 });
